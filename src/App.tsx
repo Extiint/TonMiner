e8af7ad1e3629c1729a75e775526e2454f40205c
Bonsai <manu.break7@gmail.com>
2024-05-10 16:30:19 +0400

    TWA SDK integrated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Jetton } from "./components/Jetton";
 import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
-import { CHAIN, TonConnectButton } from "@tonconnect/ui-react";
+import { CHAIN, TonConnectButton , TonConnectUIProvider , THEME} from "@tonconnect/ui-react";
 import { useTonConnect } from "./hooks/useTonConnect";
 import "@twa-dev/sdk"
 import WebApp from "@twa-dev/sdk";
@@ -30,6 +30,35 @@ function App() {
     WebApp.showAlert("Hey there!");
   };
   return (
+    <TonConnectUIProvider
+          manifestUrl="https://extiint.github.io/TonMiner/tonconnect-manifest.json"
+          uiPreferences={{ theme: THEME.DARK }}
+          walletsListConfiguration={{
+            includeWallets: [
+              {
+                appName: "safepalwallet",
+                name: "SafePal",
+                imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+                aboutUrl: "https://www.safepal.com/download",
+                jsBridgeKey: "safepalwallet",
+                platforms: ["ios", "android", "chrome", "firefox"]
+              },
+              {
+                appName: "tonwallet",
+                name: "TON Wallet",
+                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+                universalLink: "https://wallet.ton.org/ton-connect",
+                jsBridgeKey: "tonwallet",
+                bridgeUrl: "https://bridge.tonapi.io/bridge",
+                platforms: ["chrome", "android"]
+              }
+            ]
+          }}
+          actionsConfiguration={{
+              twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
+          }}
+      >
     <StyledApp>
       <AppContainer>
         <FlexBoxCol>
@@ -55,6 +84,7 @@ function App() {
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
+    </TonConnectUIProvider>
   );
 }
 
